8bd2c95add8a6c83f40f8e1f9bcada4c94c2b42a
Himanshi Singh <singhhimanshi2003@gmail.com>
2025-12-08 01:07:13 +0530

    DOne

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,9 @@ app.use(cors({
   origin: ['https://prescrepto.vercel.app','http://localhost:3000'],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "atoken"],
 }));
+console.log("CORS fix deployed");
 
 export const razorpayInstance=new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
