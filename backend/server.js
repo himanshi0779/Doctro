624592e6c70dab083a210bb097a610edb973eade
Himanshi Singh <singhhimanshi2003@gmail.com>
2025-12-07 22:59:40 +0530

    DONE

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,8 @@ app.use(express.json())
 app.use(cors({
   origin: ['https://prescrepto.vercel.app','http://localhost:3000'],
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 export const razorpayInstance=new Razorpay({
