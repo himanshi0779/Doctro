d1c4e3ff07f33f6bfd07904260ad51fbd47794cc
Himanshi Singh <singhhimanshi2003@gmail.com>
2025-12-07 22:28:49 +0530

    L

--- frontend/src/user/components/NavBar.jsx
+++ frontend/src/user/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
       {/* Logo */}
       <img
         onClick={() => navigate('/')}
-        className="h-14 w-auto sm:h-16 cursor-pointer object-contain"
+        className="h-10 w-auto sm:h-10 cursor-pointer object-contain"
         src={assets.logo}
         alt="Logo"
       />
